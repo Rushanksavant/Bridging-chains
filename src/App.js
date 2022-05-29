034f6206f175c3d73836d280ac31e54e0f1fbb9c
Rushanksavant <rushanksavant34@gmail.com>
2022-05-30 03:19:05 +0530

    first transfer

--- src/App.js
+++ src/App.js
@@ -24,74 +24,84 @@ function App() {
   const [signature, setSignature] = useState('')
   const [amount, setAmount] = useState(0)
   const [nonce, setNonce] = useState(0)
+  const [event, setEvent] = useState([])
 
-  async function getNonce() {
+  async function GetNonce() {  // needed
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
 
     const BridgeETH = new ethers.Contract(contractAddresses.Bridge_on_Eth, Bridge_on_Eth.abi, provider)
 
-    let i;
-    while (true) {
-      if (await BridgeETH.connect(account).getNonce(i)) {
-        i++
-        continue
-      } else {
-        setNonce(i)
-      }
+    let i = 0;
+    while (await BridgeETH.connect(account).getNonce(i)) {
+      i++
     }
+    // console.log(await BridgeETH.connect(account).getNonce(1))
+    setNonce(i)
+    // console.log(nonce)
   }
-  getNonce()
-  console.log(nonce)
 
-  async function SignMessage(amount) {
+  async function SignMessage(amount) {  // needed
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
 
-    let nonce = 2
-    const message = ethers.utils.solidityKeccak256(['address', 'address', 'uint256', 'uint256'],
-      [account, account, amount, nonce]).toString('hex')
-
-
+    const message = ethers.utils.solidityKeccak256(['address', 'uint256', 'uint256'],
+      [account, amount, nonce]).toString('hex')
 
     const sig = await signer.signMessage(ethers.utils.arrayify(message))
-    console.log(sig)
     setSignature(sig)
+    console.log(signature, nonce, amount)
   }
 
-  async function burn_BTT(amount) {
+  async function burn_on_ETH(amount) { // # 1
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
 
     const BridgeETH = new ethers.Contract(contractAddresses.Bridge_on_Eth, Bridge_on_Eth.abi, signer)
-    const BridgeBSC = new ethers.Contract(contractAddresses.Bridge_on_Bsc, Bridge_on_Bnb.abi, signer)
 
-    console.log(account, signature)
-    const transaction = await BridgeETH.burn(account, amount, 2, signature)
+    // console.log(account, signature, nonce)
 
+    const transaction = await BridgeETH.connect(signer).burn(amount, nonce, signature)
     await transaction.wait()
+
   }
 
-  async function mint_BTT(amount) {
+  async function queryETHevents() {
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
 
-    const BridgeETH = new ethers.Contract(contractAddresses.Bridge_on_Eth, Bridge_on_Eth.abi, signer)
-    const BridgeBSC = new ethers.Contract(contractAddresses.Bridge_on_Bsc, Bridge_on_Bnb.abi, signer)
-
-    console.log(account, signature)
-    const transaction = await BridgeBSC.mint(account, account, amount, 2, signature)
-
-    await transaction.wait()
+    const BridgeETH = new ethers.Contract(contractAddresses.Bridge_on_Eth, Bridge_on_Eth.abi, provider)
+    const eventFilter = BridgeETH.filters.Transfer(account, null, null, nonce, null, null)
+    const events = await BridgeETH.connect(account).queryFilter(eventFilter)
+    setEvent(events)
+    // console.log(events)
+    // console.log(event[0]["args"][0])
+    // console.log(ethers.utils.formatUnits(event[0]["args"][1].toString(), 'ether'))
+    // console.log(event[0]["args"][3].toString())
+    // console.log(event[0]["args"][4])
+    // const to = event[0]["args"][0]
+    setAmount(ethers.utils.formatUnits(event[0]["args"][1].toString(), 'wei'))
+    setNonce(parseInt(event[0]["args"][3].toString()))
+    setSignature(event[0]["args"][4])
   }
 
+  async function mint_on_BSC() {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const BridgeBSC = new ethers.Contract(contractAddresses.Bridge_on_Bsc, Bridge_on_Bnb.abi, signer)
+    console.log(amount, nonce, signature)
+    const transaction = await BridgeBSC.connect(signer).mint(amount, nonce, signature)
+  }
 
 
   async function connect() {
@@ -123,115 +133,65 @@ function App() {
 
       {/* Dropdown */}
       <div className="m-auto flex flex-col bg-indigo-200 text-black h-[180px] w-[300px] rounded-lg p-2 md:h-[300px] md:w-[650px] md:rounded-xl md:p-8">
-        <div>
-          <div className='flex justify-between w-full'>
-            <div className='flex flex-col'>
-              <span className='mb-2 font-semibold'>From:</span>
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                    {networkFrom === "Ropsten" ?
-                      <div className='flex'>
-                        <FaEthereum className="-mx-auto h-5 w-5" />{networkFrom}
-                      </div> :
-                      <div className='flex'>
-                        <SiBinance className="-mx-auto h-5 w-5" />{networkFrom}
-                      </div>}
-                    <IoIosArrowDown className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-                  </Menu.Button>
-                </div>
 
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
-                    <div className="py-1">
+        {/* From ETH to BSC */}
+        <div className='mx-auto flex mb-3'>
+          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white flex'
+            onClick={async () => {
+              await GetNonce() // txn nonce
+              await SignMessage(amount) // txn signature
 
-                      <Menu.Item>
-                        <button className='px-4 py-2 text-sm flex text-gray-700'
-                          onClick={() => {
-                            setNetworkFrom("Ropsten")
-                            setNetworkTo("BSC Testnet")
-                            console.log(networkFrom, networkTo)
-                          }}>
-                          <FaEthereum className="mx-auto h-5 w-5" /><span>Ropsten</span>
-                        </button>
-                      </Menu.Item>
-
-                      <Menu.Item>
-                        <button className='px-4 py-2 text-sm flex text-gray-700'
-                          onClick={() => {
-                            setNetworkFrom("BSC Testnet")
-                            setNetworkTo("Ropsten")
-                            console.log(networkFrom, networkTo)
-                          }}>
-                          <SiBinance className="mx-auto h-5 w-5" /><span>BSC Testnet</span>
-                        </button>
-                      </Menu.Item>
-                    </div>
-
-
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </div>
-
-
-
-
-            <div className='flex flex-col'>
-              <span className='mb-2 font-semibold'>To:</span>
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-
-                    {networkTo === "BSC Testnet" ?
-                      <div className='flex'>
-                        <SiBinance className="-mx-auto h-5 w-5" />{networkTo}
-                      </div> :
-                      <div className='flex'>
-                        <FaEthereum className="-mx-auto h-5 w-5" />{networkTo}
-                      </div>}
-                  </Menu.Button>
-                </div>
-
-              </Menu>
-            </div>
-          </div>
-          {/* <Dropdown account={account} /> */}
-        </div>
-
-        <div className='m-auto'>
-          <span className='font-semibold'>Amount BTT:</span>
+              await burn_on_ETH(amount)
+              await queryETHevents()
+            }}>
+            <FaEthereum className='text-xl' /><span className='ml-1'>Send</span>
+          </button>
           <input className='h-[30px] md:h-[40px] rounded-lg ml-2 p-3 bg-indigo-100'
-            onChange={(event) => setAmount(event.target.value)}>
+            onChange={(event) => setAmount(event.target.value)} placeholder="Amount BTT (10^18)">
           </input>
-          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white'
+          {/* <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white'
             onClick={() => { SignMessage(amount) }}>
             1. Sign
-          </button>
-          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white'
-            onClick={() => {
-              burn_BTT(amount)
+          </button> */}
+
+          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white flex'
+            onClick={async () => {
+              await mint_on_BSC()
             }}>
-            2. Burn
+            <SiBinance className='text-xl' /><span className='ml-1'>Recieve</span>
           </button>
-          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white'
-            onClick={() => {
-              mint_BTT(amount)
+        </div>
+
+        {/* From BSC to ETH */}
+        <div className='mx-auto flex mt-2'>
+          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white flex'
+            onClick={async () => {
+              await GetNonce() // txn nonce
+              await SignMessage(amount) // txn signature
+
+              await burn_on_ETH(amount)
+              await queryETHevents()
             }}>
-            3. Mint
+            <SiBinance className='text-xl' /><span className='ml-1'>Send</span>
+          </button>
+          <input className='h-[30px] md:h-[40px] rounded-lg ml-2 p-3 bg-indigo-100'
+            onChange={(event) => setAmount(event.target.value)} placeholder="Amount BTT (10^18)">
+          </input>
+          {/* <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white'
+            onClick={() => { SignMessage(amount) }}>
+            1. Sign
+          </button> */}
+
+          <button className='bg-red-300 p-2 ml-2 rounded-lg hover:bg-red-500 hover:text-white flex'
+            onClick={async () => {
+              await mint_on_BSC()
+            }}>
+            <FaEthereum className='text-xl' /><span className='ml-1'>Recieve</span>
           </button>
         </div>
 
         <div className='mx-auto ml-auto'>
-          <span className='font-semibold'>BTT Balance: 100</span>
+          {/* <span className='font-semibold'>BTT Balance: 100</span> */}
 
         </div>
       </div>
